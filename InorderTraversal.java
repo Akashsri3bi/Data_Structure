45b73c83beb1b3139eb044b42f99c1a09c9623c0
Akash srivastava <akashuna3@gmail.com>
2022-11-03 00:17:21 +0530

    inorderbtree

---
+++ InorderTraversal.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+import javax.swing.InputMap;
+
+public class InorderTraversal {
+
+      
+      class Node{
+            int val ; 
+            Node left , right ; 
+            Node(int data){
+                  val = data ; 
+                  left = null ; 
+                  right = null ; 
+            }
+      }
+
+      // 1st Approach which is recursive
+      public static void inorderTraversal(Node root){
+            if(root==null){
+                  return ; 
+            }
+
+            inorderTraversal(root.left);
+            System.out.print(root.val + " ");
+            inorderTraversal(root.right) ; 
+      }
+
+      // 2nd Approach which is iterative 
+      public static void iterativeInorder(Node root){
+            Stack<Node> s = new Stack<>() ; 
+
+            Node curr = root ; 
+            while(!s.isEmpty() || curr!=null){
+                  if(curr!=null){
+                        s.push(curr) ;
+                        curr = curr.left ; 
+                  }else{
+                        curr = s.pop() ; 
+                        System.out.print(curr.val + " ") ; 
+                        curr = curr.right; 
+                  }
+            }
+      }
+      
+}
