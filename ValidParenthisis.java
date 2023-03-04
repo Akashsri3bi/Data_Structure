6f56e57c65bc36dd33d05f1c5f8c2a8615018956
Akash srivastava <akashuna3@gmail.com>
2023-03-05 00:19:28 +0530

    day27

---
+++ ValidParenthisis.java
@@ -0,0 +1,55 @@
+public class ValidParenthisis {
+
+      static boolean ispar(String s)
+
+      {
+
+            Stack<Character> stack = new Stack<>();
+
+            for (char c : s.toCharArray())
+
+            {
+
+                  if (c == '(' || c == '{' || c == '[')
+
+                  {
+
+                        stack.push(c);
+
+                  } else {
+
+                        if (stack.isEmpty()) {
+
+                              return false;
+
+                        }
+
+                        else {
+
+                              char top = stack.peek();
+
+                              if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '['))
+
+                              {
+
+                                    stack.pop();
+
+                              }
+
+                              else {
+
+                                    return false;
+
+                              }
+
+                        }
+
+                  }
+
+            }
+
+            return stack.isEmpty();
+
+      }
+      
+}
