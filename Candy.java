35dd00334226f9a87315b20ff44d0c2872b20e1d
Akash srivastava <akashuna3@gmail.com>
2023-12-21 01:15:46 +0530

    232

--- Candy.java
+++ Candy.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Candy {
 
-          public int candy(int[] ratings) {
+    public int candy(int[] ratings) {
         int[] arr = new int[ratings.length] ;
         Arrays.fill(arr,1) ;  
         for(int i = 1; i<ratings.length; i++){
@@ -16,7 +16,7 @@ public class Candy {
                 arr[i] = arr[i+1] + 1;
             }
         }
-        int sum =0 ;
+        int sum = 0 ;
         for(int i = 0;i<ratings.length;i++){
             sum+=arr[i] ; 
         }
